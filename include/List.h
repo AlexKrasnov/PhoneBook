7770963ef1491f2f49d2ebfd4232ed08e42625a3
AlexKrasnov <ya.sasha-krasnov@yandex.ru>
2016-07-14 14:08:46 +0300

    HashTable

---
+++ include/List.h
@@ -0,0 +1,31 @@
+﻿#pragma once
+#include "Element.h"
+
+// класс группы хэш-таблицы в виде списка
+class List
+{
+	Element* head; // указатель на первый элемент списка
+public:
+	// конструктор создания пустого списка
+	List();
+	// деструктор списка
+	~List();
+	bool IsEmpty() { return head == NULL; }
+	// метод добавления в конец новой записи об абоненте
+	void PushAbonent(char*, char*);
+	// метод удаления из списка по имени абонента
+	void DeleteAbonent(char*);
+	// метод поиска элемента списка по имени абонента
+	bool FindAbonent(char*, Abonent&);
+	// дружественные функции вывода
+	friend ostream& operator << (ostream& os, List& l)
+	{
+		Element* tmp = l.head;
+		while (tmp != NULL)
+		{
+			os << tmp->GetAbonent();
+			tmp = tmp->GetNext();
+		}
+		return os;
+	}
+};
